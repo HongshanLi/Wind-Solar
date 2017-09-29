3358f56cc15c196a1926aa6f690b92be7327a160
lihongshan <lihongshan1989@live.cn>
2017-09-29 14:18:58 -0400

    grid data

--- SolarEnergyPrediction/Mesonet.py
+++ SolarEnergyPrediction/Mesonet.py
@@ -1,28 +1,65 @@
 from netCDF4 import Dataset
 import numpy as np
 import os
+from scipy import interpolate
+import matplotlib.pyplot as plt
+
 
 class WeatherData():
-    def __init__(self, data_loc):
+    def __init__(self, data_loc="/home/hongshan/Dataset/SolarEnergy"):
         # Use a sample variable to initialize some attributes
-        sample_var = os.listidr(data_loc)[0]
+        sample_var = os.listdir(data_loc)[0]
         sample_var = Dataset(os.path.join(data_loc, sample_var))
-        time = 
+        self.time = sample_var.variables["time"]
+        self.lat = sample_var.variables["lat"]
+        self.lon = sample_var.variables["lon"]
+        self.fhour = sample_var.variables["fhour"]
 
         # load all nc files
-        self.all_variables = []
-        for var in os.listdir():
+        """ 
+        self.all_features = []
+        for var in os.listdir(data_loc):
             var_path = os.path.join(data_loc, var)
             dataset = Dataset(var_path)
-            values = dataset.variables.values
+            values = dataset.variables.values()[-1]
+            values = np.array(values)
+            values = values.mean(axis=1) # take mean of ensemble of models
+            self.all_features.append(values)
+        """
         
 
-    def _check_index()
-
-
-    def location(self, lat, lon):
-        # Arguments: actual values of lattitude and longitude (not index)
-        # 
+    def _check_index(self, lat, lon):
+        lat_idx = np.where(self.lat.__array__() == lat)
+        lon_idx = np.where(self.lon.__array__() == lon)
+        return lat_idx, lon_idx
         
-        return (lat, lon), data_at_this_location
+    def _get_single_variable(self, var, lat, lon):
+        lat_idx, lon_idx = self._check_index(lat, lon)
+        return var[:,:,lat_idx,lon_idx].reshape(1,-1)
+    
+    def _get_all_variables(self, lat, lon):
+        all_vars = []
+        for var in self.all_features:
+            all_vars.append(self._get_single_variable(var, lat, lon))
+        return np.concatenate(all_vars, axis=0).transpose()
+
+    def _make_grid(self):
+        grid_x = np.concatenate([self.lon.__array__()]*len(self.lat.__array__()))
+        y = self.lat.__array__()
+        y = y.reshape(1, -1)
+        grid_y = np.concatenate([y]*len(self.lon.__array__()), axis=0)
+        grid_y = grid_y.flatten("F")
+        plt.scatter(x=grid_x, y=grid_y, color="b")
+        plt.show()
+
+
+w = WeatherData()
+print(w.lat.__array__().shape)
+print(w.lon.__array__())
+print(w.time.__array__())
+w._make_grid()
+        
+
+
+        
 
