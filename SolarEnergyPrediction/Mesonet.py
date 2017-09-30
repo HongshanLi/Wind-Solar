29bfefe7f21773de0c0e35022b329c9076fb03bb
lihongshan <lihongshan1989@live.cn>
2017-09-29 21:24:53 -0400

    interpolation done in scratch.ipynb

--- SolarEnergyPrediction/Mesonet.py
+++ SolarEnergyPrediction/Mesonet.py
@@ -16,7 +16,6 @@ class WeatherData():
         self.fhour = sample_var.variables["fhour"]
 
         # load all nc files
-        """ 
         self.all_features = []
         for var in os.listdir(data_loc):
             var_path = os.path.join(data_loc, var)
@@ -25,7 +24,6 @@ class WeatherData():
             values = np.array(values)
             values = values.mean(axis=1) # take mean of ensemble of models
             self.all_features.append(values)
-        """
         
 
     def _check_index(self, lat, lon):
@@ -33,14 +31,14 @@ class WeatherData():
         lon_idx = np.where(self.lon.__array__() == lon)
         return lat_idx, lon_idx
         
-    def _get_single_variable(self, var, lat, lon):
+    def _get_single_variable(self, var, time, lat, lon):
         lat_idx, lon_idx = self._check_index(lat, lon)
-        return var[:,:,lat_idx,lon_idx].reshape(1,-1)
+        return var[time,:,lat_idx,lon_idx].flatten()
     
-    def _get_all_variables(self, lat, lon):
+    def _get_all_variables(self, time, lat, lon):
         all_vars = []
         for var in self.all_features:
-            all_vars.append(self._get_single_variable(var, lat, lon))
+            all_vars.append(self._get_single_variable(var, time,lat, lon))
         return np.concatenate(all_vars, axis=0).transpose()
 
     def _make_grid(self):
@@ -52,12 +50,18 @@ class WeatherData():
         plt.scatter(x=grid_x, y=grid_y, color="b")
         plt.show()
 
+    def interpolate(self, time, lat, lon, kind):
+        x_coord = self.lon.__array__()
+        y_coord = self.lat.__array__()
+        xx, yy = np.meshgrid(x_coord, y_coord)
+        # I need 2d array of data here
+        
 
-w = WeatherData()
-print(w.lat.__array__().shape)
-print(w.lon.__array__())
-print(w.time.__array__())
-w._make_grid()
+
+if __name__ =="__main__":
+    w = WeatherData()
+    print(w._get_single_variable(w.all_features[0], 31, 260).shape)
+    
         
 
 
